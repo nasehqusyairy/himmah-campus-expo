798c026f94006dbfab0b5c4f943655fb0a43e6d4
Mohammad Nasyikh Al-Qusyairy <nasyikhqusyairy@gmail.com>
2026-01-10 12:11:16 +0700

    fix(validating): add optional chaining for invoice properties in InvoicePreviewDialog

--- resources/js/components/invoice-preview-dialog.tsx
+++ resources/js/components/invoice-preview-dialog.tsx
@@ -54,13 +54,13 @@ export default function InvoicePreviewDialog({ users, setUsers, userIndex, setUs
                                 <TableCell>
                                     No. Whatsapp
                                 </TableCell>
-                                <TableCell>: <a target="_blank" className="text-primary underline" href={"https://wa.me/+62" + user?.invoice.wa}>{user?.invoice.wa}</a></TableCell>
+                                <TableCell>: <a target="_blank" className="text-primary underline" href={"https://wa.me/+62" + user?.invoice?.wa}>{user?.invoice?.wa}</a></TableCell>
                             </TableRow>
                             <TableRow>
                                 <TableCell>
                                     Jenis Instansi
                                 </TableCell>
-                                <TableCell>: {user?.invoice.agency?.level.name}</TableCell>
+                                <TableCell>: {user?.invoice?.agency?.level.name}</TableCell>
                             </TableRow>
                             <TableRow>
                                 <TableCell>
@@ -68,7 +68,7 @@ export default function InvoicePreviewDialog({ users, setUsers, userIndex, setUs
                                 </TableCell>
                                 <TableCell className="flex items-center gap-1">
                                     <span>:</span>
-                                    <span className="inline-block w-44 text-wrap">{user?.invoice.agency?.name}</span>
+                                    <span className="inline-block w-44 text-wrap">{user?.invoice?.agency?.name}</span>
                                 </TableCell>
                             </TableRow>
                             <TableRow>
@@ -77,7 +77,7 @@ export default function InvoicePreviewDialog({ users, setUsers, userIndex, setUs
                             <TableRow>
                                 <TableCell colSpan={2}>
                                     <ol className="list-decimal pl-12">
-                                        {user?.invoice.participants.map(el => (
+                                        {user?.invoice?.participants.map(el => (
                                             <li className="mb-2" key={el.id}>{el.name}</li>
                                         ))}
                                     </ol>
@@ -88,8 +88,8 @@ export default function InvoicePreviewDialog({ users, setUsers, userIndex, setUs
                             </TableRow>
                             <TableRow>
                                 <TableCell colSpan={2}>
-                                    {user?.invoice.payment_file ? (
-                                        <img src={paymentFile().url + '?path=' + user?.invoice.payment_file} alt="" />
+                                    {user?.invoice?.payment_file ? (
+                                        <img src={paymentFile().url + '?path=' + user?.invoice?.payment_file} alt="" />
 
                                     ) : (
                                         <div className="border-dashed border rounded-md aspect-video flex justify-center items-center">
@@ -101,7 +101,7 @@ export default function InvoicePreviewDialog({ users, setUsers, userIndex, setUs
                         </TableBody>
                     </Table>
                 </div>
-                {(!user?.invoice.verified_at && user?.step.last === 3) && (
+                {(!user?.invoice?.verified_at && user?.step?.last === 3) && (
                     <DialogFooter>
                         <AlertDialog open={isRejctDialogOpen} onOpenChange={setIsRejctDialogOpen}>
                             <AlertDialogTrigger asChild>
@@ -121,11 +121,11 @@ export default function InvoicePreviewDialog({ users, setUsers, userIndex, setUs
                                     <Button disabled={isLoading} onClick={() => {
                                         setIsLoading(true)
                                         axios.post(reject().url, {
-                                            invoice_id: user.invoice.id
+                                            invoice_id: user.invoice?.id
                                         }).then(({ data: res }) => {
                                             toast.success(res.message)
                                             const newUsers = [...users]
-                                            newUsers[userIndex!].step.last = 0
+                                            newUsers[userIndex!].step!.last = 0
                                             setUsers(newUsers)
                                             setUserIndex(undefined)
                                         }).catch(err => {
@@ -166,11 +166,11 @@ export default function InvoicePreviewDialog({ users, setUsers, userIndex, setUs
                                     <Button disabled={isLoading} onClick={() => {
                                         setIsLoading(true)
                                         axios.post(accept().url, {
-                                            invoice_id: user.invoice.id
+                                            invoice_id: user.invoice?.id
                                         }).then(({ data: res }) => {
                                             toast.success(res.message)
                                             const newUsers = [...users]
-                                            newUsers[userIndex!].invoice.verified_at = res.data.verified_at
+                                            newUsers[userIndex!].invoice!.verified_at = res.data.verified_at
                                             setUsers(newUsers)
                                             setUserIndex(undefined)
                                         }).catch(err => {
